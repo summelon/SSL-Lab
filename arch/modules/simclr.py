9e4d60341dc30665fafc2f473053ee817fc6bd90
summelon <summelon7@gmail.com>
2021-07-03 15:34:18 +0800

    Add SimCLR

---
+++ arch/modules/simclr.py
@@ -0,0 +1,58 @@
+import lightly
+from omegaconf import DictConfig
+
+from .base import BaseModel
+from ..models.simsiam_arm import SiameseArm
+
+
+class SimCLRModel(BaseModel):
+    def __init__(
+        self,
+        basic: DictConfig,
+        backbone: DictConfig,
+        mlp: DictConfig,
+        optimizer: DictConfig,
+        scheduler: DictConfig,
+    ):
+        super().__init__()
+        self.save_hyperparameters()
+        self.online_network = self._prepare_model()
+        self.criterion = lightly.loss.NTXentLoss(
+            temperature=self.hparams.basic.temperature,
+            memory_bank_size=0,
+        )
+        self.outputs = None
+
+    def forward(self, x):
+        # Input: x0, x1, return_features
+        # Output: ((projection, prediction), backbone_feature)
+        _, backbone_feature = self.online_network(x, return_features=True)
+        return backbone_feature
+
+    def training_step(self, batch, batch_idx):
+        # (x0, x1), _, _ = batch
+        # (Aug0, Aug1, w/o aug), label
+        (x0, x1, _), _ = batch
+        y0, y1 = self.online_network(x0, x1)
+        loss = self.criterion(y0, y1)
+        # TODO: modify to more clean method
+        self.outputs = y0
+        self.log(
+            "repr_loss", loss, prog_bar=True, logger=True,
+            on_step=True, on_epoch=False, sync_dist=True,
+        )
+        return loss
+
+    def _prepare_model(self):
+        online_network = SiameseArm(
+            backbone=self.hparams.backbone.backbone,
+            first_conv=self.hparams.backbone.first_conv,
+            maxpool1=self.hparams.backbone.maxpool1,
+            proj_hidden_dim=self.hparams.mlp.proj_hidden_dim,
+            pred_hidden_dim=self.hparams.mlp.pred_hidden_dim,
+            out_dim=self.hparams.mlp.out_dim,
+            num_proj_mlp_layer=self.hparams.mlp.num_proj_mlp_layer,
+            proj_last_bn=self.hparams.mlp.proj_last_bn,
+            using_predictor=self.hparams.mlp.using_predictor,
+        )
+        return online_network
