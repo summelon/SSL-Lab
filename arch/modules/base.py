Fix SwAV scores wrong name; fix SimEstimator target forward with gradient; fix self.outputs wrong assign

arch/modules/base.py
@@ -121,8 +121,9 @@ class BaseModel(pl.LightningModule):
         # Global features
         # features = (proj_features, pred_features)
         features = network(torch.cat(images[:g_crops]))
-        # Outputs for collapse checker
-        self.outputs = features[0]
+        # Outputs for collapse checker(local_forward in online net)
+        if local_forward:
+            self.outputs = features[0]
         features = features[feat_idx]
 
         # Local features if have local crops

arch/modules/sim_estimator.py
@@ -64,6 +64,7 @@ class SimEstimatorModel(BaseModel):
             local_forward=True,
             use_projector_feature=False,
         )
+        online_feat = self.prototypes(online_feat)
         # Target
         with torch.no_grad():
             target_feat = self._multi_crop_forward(
@@ -73,7 +74,8 @@ class SimEstimatorModel(BaseModel):
                 # Asymmetric forward like BYOL
                 use_projector_feature=True,
             )
-        return self.prototypes(online_feat), self.prototypes(target_feat)
+            target_feat = self.prototypes(target_feat)
+        return online_feat, target_feat
 
     def training_step(self, batch, batch_idx):
         images, labels = batch

arch/modules/swav.py
@@ -87,18 +87,18 @@ class SwAVModel(BaseModel):
         self,
         index: int,
         features: torch.Tensor,
-        online_score: torch.Tensor,
+        online_scores: torch.Tensor,
     ) -> torch.Tensor:
 
         bs = self.hparams.basic.device_batch_size
         # forward if queue is all filled
         if torch.all(self.queue[index, -1, :] != 0):
             queue_scores = self.prototypes(self.queue[index])
-            queue_scores = torch.cat((queue_scores, online_score))
+            online_scores = torch.cat((queue_scores, online_scores))
         # Pop the oldest batch and update
         self.queue[index, bs:] = self.queue[index, :-bs].clone()
         self.queue[index, :bs] = features[bs*index: bs*(index+1)]
-        return queue_scores
+        return online_scores
 
     @torch.no_grad()
     def _get_assignments(self, features: torch.Tensor, scores: torch.Tensor):